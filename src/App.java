213c19f0c7ea78813fb8ae198769a73e848ae50f
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2024-04-03 16:12:18 -0300

    update

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 
 //Recursividade
 
+/**
+  * Crie um Metodo chamdo findElement que seja capaz de encontra o enesimo
+  * elemento de uma sequancia de fibonacci.
+  * 
+  * input: n, um inteiro representando a posicao do elemento na sequancia.
+  * output: O enesimo elemento da sequancia de fibonacci.
+  * seuqncia: 0, 1, 1, 2, 3, 5
+  */
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,24 +17,19 @@ public class App {
     // Mapa para armazenar os números Fibonacci já calculados
     private static Map<Integer, Integer> memo = new HashMap<>();
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(findElement(30));
-        memo.forEach((chave, valor) -> System.out.println("Chave: " + chave + ", Valor: " + valor));
+    public static int findElement(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        // 2 = elemento na posicao 1 + elemento na posicao 0
+        // 2 = findElement(1) + findElement(0)
+        // 3 = elemento na posicao 2 + elemento na posicao 1
+        // 3 = findElement(2) + findElement(1)
+        return findElement(n - 1) + findElement(n - 2);
     }
 
-    public static int findElement(int numeroElemento) {
-        if (numeroElemento <= 1) {
-            return numeroElemento;
-        }
-
-        // Verifica se o valor já foi calculado e armazenado no memo
-        if (memo.containsKey(numeroElemento)) {
-            return memo.get(numeroElemento);
-        }
-
-        // Chamadas recursivas com memoização
-        int resultado = findElement(numeroElemento - 1) + findElement(numeroElemento - 2);
-        memo.put(numeroElemento, resultado); // Armazena o resultado no memo
-        return resultado;
+    public static void main(String[] args) throws Exception {
+        int n = 30;
+        System.err.println("Elemento " + n + " : " + findElement(n));
     }
 }
